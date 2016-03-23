cc1ece3c063973fadd5e0c3fd804ba847047bb68
Rob van den Berg <rvdberg757@gmail.com>
2016-03-23 12:02:44 +0100

    Added files via upload
    adding jap

---
+++ jap.sql
@@ -0,0 +1,71 @@
+@pck jap
+
+begin
+ --      1
+ --      1 2 1 3
+ --      -------
+ -- 2 1 |O O   O
+ -- 1 1 |  O   O
+ -- 1 2 |O   O O
+ --
+  jap.oplossen
+  ( jap.diagram
+    ( jap.dimensie
+      ( jap.patroon (2,1)
+      , jap.patroon (1,1)
+      , jap.patroon (1,2)
+      )
+    , jap.dimensie
+      ( jap.patroon (1,1)
+      , jap.patroon (2)
+      , jap.patroon (1)
+      , jap.patroon (3)
+      )
+    )
+  );
+  -- Puzzelsport 'Japanse Puzzel Mix' juli 2006 nr 3: 'netwerker'
+--   jap.oplossen
+--   ( jap.diagram
+--     ( jap.dimensie
+--       ( jap.patroon (4)
+--       , jap.patroon (4,1)
+--       , jap.patroon (4,2)
+--       , jap.patroon (5,1,3)
+--       , jap.patroon (3,1,1,1,2)
+--       , jap.patroon (1,2,1,1)
+--       , jap.patroon (6,1,1,1)
+--       , jap.patroon (1,1,1,1,1,1)
+--       , jap.patroon (5,3,1)
+--       , jap.patroon (1,1,1,6)
+--       , jap.patroon (3,5)
+--       , jap.patroon (1,2,3,2)
+--       , jap.patroon (1,1,1,3)
+--       , jap.patroon (8)
+--       , jap.patroon (5)
+--       , jap.patroon (1,1)
+--       , jap.patroon (1,1,1)
+--       , jap.patroon (1,2,1)
+--       , jap.patroon (1,2,3)
+--       , jap.patroon (1,1,1,1)
+--       )
+--     , jap.dimensie
+--       ( jap.patroon (4,1)
+--       , jap.patroon (1,1,1,1)
+--       , jap.patroon (3,6)
+--       , jap.patroon (5,1,1,3)
+--       , jap.patroon (5,4)
+--       , jap.patroon (5,1)
+--       , jap.patroon (1,1)
+--       , jap.patroon (2,2,2,2)
+--       , jap.patroon (2,2,1,1,2)
+--       , jap.patroon (2,2,1,1,1,2)
+--       , jap.patroon (7,2,2)
+--       , jap.patroon (1,3,2,2)
+--       , jap.patroon (1,1,2,3,2)
+--       , jap.patroon (2,7,1)
+--       , jap.patroon (5,11)
+--       )
+--     )
+--   );
+end;
+/
